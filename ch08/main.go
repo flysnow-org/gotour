986f57d10b848cf0fce70280a11d7abb7911ce1d
飞雪无情 <ls8707@gmail.com>
2020-11-29 16:14:46 +0800

    add ch08,ch09 sourcecode

---
+++ ch08/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := make(chan string)
+
+	go func() {
+		fmt.Println("飞雪无情")
+		ch <- "goroutine 完成"
+	}()
+
+	fmt.Println("我是 main goroutine")
+
+	v := <-ch
+	fmt.Println("接收到的chan中的值为：", v)
+
+	cacheCh := make(chan int, 5)
+	cacheCh <- 2
+	cacheCh <- 3
+	fmt.Println("cacheCh容量为:", cap(cacheCh), ",元素个数为：", len(cacheCh))
+
+	firstCh := make(chan string)
+	secondCh := make(chan string)
+	threeCh := make(chan string)
+
+	go func() {
+		firstCh <- downloadFile("firstCh")
+	}()
+
+	go func() {
+		secondCh <- downloadFile("secondCh")
+	}()
+
+	go func() {
+		threeCh <- downloadFile("threeCh")
+	}()
+
+	select {
+	case filePath := <-firstCh:
+		fmt.Println(filePath)
+	case filePath := <-secondCh:
+		fmt.Println(filePath)
+	case filePath := <-threeCh:
+		fmt.Println(filePath)
+	}
+}
+
+func downloadFile(chanName string) string {
+	//模拟下载文件,可以自己随机time.Sleep点时间试试
+	 time.Sleep(time.Second)
+	return chanName+":filePath"
+}
---
+++ ch09/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var(
+	sum int
+	mutex sync.RWMutex
+)
+
+func main() {
+	run()
+	doOnce()
+	race()
+	syncMap()
+}
+
+func run(){
+	var wg sync.WaitGroup
+	//因为要监控110个协程，所以设置计数器为110
+	wg.Add(110)
+	for i := 0; i < 100; i++ {
+		go func() {
+			//计数器值减1
+			defer wg.Done()
+			add(10)
+		}()
+	}
+	for i:=0; i<10;i++ {
+		go func() {
+			//计数器值减1
+			defer wg.Done()
+			fmt.Println("和为:",readSum())
+		}()
+	}
+	//一直等待，只要计数器值为0
+	wg.Wait()
+}
+
+func doOnce() {
+	var once sync.Once
+	onceBody := func() {
+		fmt.Println("Only once")
+	}
+	done := make(chan bool)
+	for i := 0; i < 10; i++ {
+		go func() {
+			once.Do(onceBody)
+			done <- true
+		}()
+	}
+	for i := 0; i < 10; i++ {
+		<-done
+	}
+}
+
+//10个人赛跑,1个裁判发号施令
+func race(){
+	cond :=sync.NewCond(&sync.Mutex{})
+	var wg sync.WaitGroup
+	wg.Add(11)
+	for i:=0;i<10; i++ {
+		go func(num int) {
+			defer  wg.Done()
+			fmt.Println(num,"号已经就位")
+			cond.L.Lock()
+			cond.Wait()//等待发令枪响
+			fmt.Println(num,"号开始跑……")
+			cond.L.Unlock()
+		}(i)
+	}
+	//等待所有goroutine都进入wait状态
+	time.Sleep(2*time.Second)
+	go func() {
+		defer  wg.Done()
+		fmt.Println("裁判已经就位，准备发令枪")
+		fmt.Println("比赛开始，大家准备跑")
+		cond.Broadcast()//发令枪响
+	}()
+	wg.Wait()
+}
+
+func syncMap(){
+	syncMap:=sync.Map{}
+	syncMap.Store(1,1)
+	syncMap.Store(1,2)
+	fmt.Println(syncMap.Load(1))
+}
+
+func add(i int) {
+	mutex.Lock()
+	defer mutex.Unlock()
+	sum += i
+}
+
+func readSum() int {
+	mutex.RLock()
+	defer mutex.RUnlock()
+	b:=sum
+	return b
+}
