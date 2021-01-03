623febc04de5bc3b30e3f9b50b4f4bd8e81cd1dd
飞雪无情 <ls8707@gmail.com>
2021-01-03 22:33:43 +0800

    ch22

--- ch22/client_main.go
+++ ch22/client_main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"gotour/ch22/server"
+	"io"
 	"log"
+	"net"
+	"net/http"
+	"net/rpc"
 	"net/rpc/jsonrpc"
 )
 
 func main()  {
-	client, err := jsonrpc.Dial("tcp",  "localhost:1234")
+	client, err := DialHTTP("tcp",  "localhost:1234")
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
@@ -20,4 +26,39 @@ func main()  {
 		log.Fatal("MathService.Add error:", err)
 	}
 	fmt.Printf("MathService.Add: %d+%d=%d", args.A, args.B, reply)
+}
+
+// DialHTTP connects to an HTTP RPC server at the specified network address
+// listening on the default HTTP RPC path.
+func DialHTTP(network, address string) (*rpc.Client, error) {
+	return DialHTTPPath(network, address, rpc.DefaultRPCPath)
+}
+
+// DialHTTPPath connects to an HTTP RPC server
+// at the specified network address and path.
+func DialHTTPPath(network, address, path string) (*rpc.Client, error) {
+	var err error
+	conn, err := net.Dial(network, address)
+	if err != nil {
+		return nil, err
+	}
+	io.WriteString(conn, "GET "+path+" HTTP/1.0\n\n")
+
+	// Require successful HTTP response
+	// before switching to RPC protocol.
+	resp, err := http.ReadResponse(bufio.NewReader(conn), &http.Request{Method: "GET"})
+	connected := "200 Connected to JSON RPC"
+	if err == nil && resp.Status == connected {
+		return jsonrpc.NewClient(conn), nil
+	}
+	if err == nil {
+		err = errors.New("unexpected HTTP response: " + resp.Status)
+	}
+	conn.Close()
+	return nil, &net.OpError{
+		Op:   "dial-http",
+		Net:  network + " " + address,
+		Addr: nil,
+		Err:  err,
+	}
 }
--- ch22/server_main.go
+++ ch22/server_main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"gotour/ch22/server"
+	"io"
 	"log"
 	"net"
+	"net/http"
 	"net/rpc"
 	"net/rpc/jsonrpc"
 )
@@ -11,18 +13,22 @@ import (
 func main() {
 	rpc.RegisterName("MathService", new(server.MathService))
 
+	//注册一个path，用于提供基于http的json rpc服务
+	http.HandleFunc(rpc.DefaultRPCPath, func(rw http.ResponseWriter, r *http.Request) {
+		conn, _, err := rw.(http.Hijacker).Hijack()
+		if err != nil {
+			log.Print("rpc hijacking ", r.RemoteAddr, ": ", err.Error())
+			return
+		}
+		var connected = "200 Connected to JSON RPC"
+		io.WriteString(conn, "HTTP/1.0 "+connected+"\n\n")
+		jsonrpc.ServeConn(conn)
+	})
+
 	l, e := net.Listen("tcp", ":1234")
 	if e != nil {
 		log.Fatal("listen error:", e)
 	}
 
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			log.Println("jsonrpc.Serve: accept:", err.Error())
-			return
-		}
-		//json rpc关键点
-		go jsonrpc.ServeConn(conn)
-	}
+	http.Serve(l, nil)//换成http的服务
 }
